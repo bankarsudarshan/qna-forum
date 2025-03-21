7bc706efde68f9edce6bfcfae212a3b22c37c414
Sudarshan Bankar <sudarshanbankar22@gmail.com>
2025-03-21 13:51:31 +0530

    changed database.json file

--- src/config/database.js
+++ src/config/database.js
@@ -7,23 +7,23 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "postgres",  // or "mysql" if using MySQL
+    dialect: "postgres",
     logging: (msg) => logger.info(msg),
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_TEST_USERNAME,
+    password: process.env.DB_TEST_PASSWORD,
+    database: process.env.DB_TEST_NAME,
+    host: process.env.DB_TEST_HOST,
     dialect: "postgres",
-    logging: false
+    // Disable logging in tests
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_PROD_USERNAME,
+    password: process.env.DB_PROD_PASSWORD,
+    database: process.env.DB_PROD_NAME,
+    host: process.env.DB_PROD_HOST,
     dialect: "postgres",
-    logging: false
-  }
+    // Disable logs in production for security
+  },
 };
